bdce6105483ef0f73ced0e67fdb1318c9c91a582
nbakh16 <nbakh16@gmail.com>
2024-09-29 13:31:23 +0600

    image alignment fixed

--- lib/app/widgets/custom_network_image.dart
+++ lib/app/widgets/custom_network_image.dart
@@ -14,60 +14,60 @@ class CustomNetworkImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
           topRight: Radius.circular(6.0),
           topLeft: Radius.circular(6.0),
-        )
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: CachedNetworkImage(
-        key: UniqueKey(),
-        imageUrl: imgUrl,
-        imageBuilder: (context, imageProvider) => Container(
-          height: double.infinity,
-          width: double.infinity,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover
-            )
-          ),
-        ),
-        progressIndicatorBuilder: (context, url, downloadProgress) =>
-            Center(
-              child: CircularProgressIndicator(value: downloadProgress.progress)
+        )),
+        clipBehavior: Clip.hardEdge,
+        child: CachedNetworkImage(
+          key: UniqueKey(),
+          imageUrl: imgUrl,
+          imageBuilder: (context, imageProvider) => Container(
+            height: double.infinity,
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: imageProvider,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
               ),
-        errorWidget: (_, __, ___) => Icon(Icons.image, size: 40, color: mainColor.shade300,),
-        cacheManager: CacheManager(
-            Config(
-              'customCacheKey',
-              stalePeriod: const Duration(days: 7),
-              maxNrOfCacheObjects: 124,
-            )
-        ),
-      )
+            ),
+          ),
+          progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+              child:
+                  CircularProgressIndicator(value: downloadProgress.progress)),
+          errorWidget: (_, __, ___) => Icon(
+            Icons.image,
+            size: 40,
+            color: mainColor.shade300,
+          ),
+          cacheManager: CacheManager(Config(
+            'customCacheKey',
+            stalePeriod: const Duration(days: 7),
+            maxNrOfCacheObjects: 124,
+          )),
+        )
 
-
-      // Image.network(imgUrl,
-      //   alignment: Alignment.center,
-      //   height: double.infinity,
-      //   width: double.infinity,
-      //   fit: BoxFit.cover,
-      //   loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loading) {
-      //     if (loading == null) return child;
-      //     return Center(
-      //       child: CircularProgressIndicator(
-      //         value: loading.expectedTotalBytes != null
-      //             ? loading.cumulativeBytesLoaded/loading.expectedTotalBytes! : null,
-      //       ),
-      //     );
-      //   },
-      //   errorBuilder: (_, __, ___) {
-      //     return Icon(Icons.image, size: 40, color: mainColor.shade300,);
-      //   }
-      // ),
-    );
+        // Image.network(imgUrl,
+        //   alignment: Alignment.center,
+        //   height: double.infinity,
+        //   width: double.infinity,
+        //   fit: BoxFit.cover,
+        //   loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loading) {
+        //     if (loading == null) return child;
+        //     return Center(
+        //       child: CircularProgressIndicator(
+        //         value: loading.expectedTotalBytes != null
+        //             ? loading.cumulativeBytesLoaded/loading.expectedTotalBytes! : null,
+        //       ),
+        //     );
+        //   },
+        //   errorBuilder: (_, __, ___) {
+        //     return Icon(Icons.image, size: 40, color: mainColor.shade300,);
+        //   }
+        // ),
+        );
   }
 }
